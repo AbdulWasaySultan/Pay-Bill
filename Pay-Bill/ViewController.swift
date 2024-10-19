45b0f7d9a0ae92e631676e57521140ba216cca2f
Abdul Wasay <abdulwasay1408@gmail.com>
2024-10-19 16:31:16 +0500

    Initial Commit

---
+++ Pay-Bill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pay-Bill
+//
+//  Created by ABDUR RAFAY on 19/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pay-BillUITests/Pay_BillUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pay_BillUITestsLaunchTests.swift
+//  Pay-BillUITests
+//
+//  Created by ABDUR RAFAY on 19/10/2024.
+//
+
+import XCTest
+
+final class Pay_BillUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
